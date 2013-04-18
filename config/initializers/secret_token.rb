21e48b12fbaafa728115c24bc264c0b00a861280
Gursimran Singh <g13singh@gmail.com>
2013-04-18 18:30:16 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PromiscuousChemist::Application.config.secret_token = 'ee1b09d01169c80fb5e313e103259d6346f647aafce79d8de976e575cf7041c239c359877fa3fdf2fefefc055ee27548762f0469bc351404d2e0acbe6555d734'
