d1c4a3f071179c765c14046aae979f9ec16c2d86
Gursimran Singh <g13singh@gmail.com>
2013-04-18 19:47:34 -0500

    init

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    out = {}.to_json
+    if params[:url]
+      # uri_enc_url = Rack::Utils.escape(params[:url])
+      endpoint = "http://access.alchemyapi.com/calls/url/URLGetCategory"
+      q = "#{endpoint}?apikey=#{ENV["ALCHEMY_KEY"]}&url=#{params[:url]}&outputMode=json"
+      p q
+      out = RestClient.get(q)
+    end
+    respond_to do |format|
+      format.html
+      format.json { render :json => out.body }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 PromiscuousChemist::Application.routes.draw do
   root :to => "home#index"
+  match "categorize" => "home#index"
 end
